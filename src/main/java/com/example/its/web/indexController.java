5c6728291988799b90b99eb5bb4918aaf20948f3
yu ji <leon5617@gmail.com>
2024-01-31 01:44:48 +0900

    toppageの表示をテンプレートエンジンに変更した。

--- src/main/java/com/example/its/web/indexController.java
+++ src/main/java/com/example/its/web/indexController.java
@@ -2,15 +2,13 @@ package com.example.its.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class indexController {
 
   @GetMapping("/")
-  @ResponseBody //戻り値のStringをResponseする。
   public String index() {
-    return "<h1>Hello,World!</h1>";
+    return "index"; //index.html
   }
 
 }
